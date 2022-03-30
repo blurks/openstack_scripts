e08dcc7008f2b0c17b0b7162184e3d227fd10197
Tarik Havighorst <code@blurks.de>
2022-03-30 13:01:55 +0200

    parse json output

--- setup_test_vm.sh
+++ setup_test_vm.sh
@@ -4,6 +4,9 @@
 # creating new vm everytime, create one beforehand and boot from a snapshot
 # everytime we want to run a test
 
+# logfile. leave empty to disable logging
+LOGFILE=./server_creation.log
+
 # ---- User authentication.
 # cf. https://docs.openstack.org/python-openstackclient/latest/cli/man/openstack.html
 
@@ -42,12 +45,19 @@ INSTANCE_NAME=
 # ---- Create the server
 # cf. https://docs.openstack.org/nova/xena/user/launch-instances.html
 
-openstack server create \
-		  --flavor $FLAVOR_ID \
-		  --image $IMAGE_ID \
-		  --key-name $KEY_NAME \
-		  --security-group $SEC_GROUP_NAME \
-		  "$INSTANCE_NAME"
+echo "creating server" | tee $LOGFILE
+SERVER_ID=$(openstack server create \
+					  -f json \
+					  --flavor $FLAVOR_ID \
+					  --image $IMAGE_ID \
+					  --key-name $KEY_NAME \
+					  --security-group $SEC_GROUP_NAME \
+					  --wait \
+					  "$INSTANCE_NAME" \
+				| tee -a $LOGFILE \
+				| jq '.id'
+		 )
+
 
 # You can check if the server was created using `openstack server list`
 
@@ -60,18 +70,30 @@ openstack server create \
 
 # allocate an ip address
 # TODO: check if extraction of ip address works
-IP_ADDRESS=`openstack floating ip create public | grep floating_ip_address | grep -oP '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'`
 
-# get server id
-# TODO: extract id from output
-## SERVER_ID=`openstack server list --instance-name "$INSTANCE_NAME" | so_some_grep_magic`
+echo "allocating ip address" | tee -a $LOGFILE
+IP_ADDRESS=$(openstack floating ip create -f json  "public" \
+				 | tee -a $LOGFILE \
+				 | jq '.floating_ip.ip')
 
 # get port id
-## PORT_ID=`openstack port list --device-id $SERVER_ID | do_some_grep_magic`
+echo "get portid" | tee -a $LOGFILE
+PORT_ID=$(openstack port list -f json --device-id $SERVER_ID \
+			  | tee -a $LOGFILE \
+			  | jq '.interfaceAttachments[0].port_id'
+	   )
 
 # associate ip with port
-openstack floating ip set --port $PORT_ID $IP_ADDRESS
+echo "associate ip with port" | tee -a $LOGFILE
+openstack floating ip set --port $PORT_ID $IP_ADDRESS | tee -a $LOGFILE
 
+
+
+# ---- Do the deployment
+
+openstack server start $SERVER_ID
+
+fab -u cloud -H $IP_ADDRESS deploy:staging
 
 # ---- Cleanup
 
